e055514616e7528262b3805498b7fea614aa609d
Alexey Agapitov <marwinxxii@gmail.com>
2011-03-20 00:52:10 +0300

    ktope is now located in own module fixed coloring graph

--- ktope_hw2.py
+++ ktope_hw2.py
@@ -1,8 +1,16 @@
+import collections
+
+__name__='ktope_hw2'
+
 def buildCircuits(lines):
+    """Iterate over lines and build dict of circuits"""
+    
     #dict because we need to store numbers of circuits
     circuits={}
     i=1
     for line in lines:
+        if line=='':
+            continue
         #using set to avoid duplicate elements
         circuit=set()
         for element in line.split(','):
@@ -14,46 +22,106 @@ def buildCircuits(lines):
     return circuits
 
 def buildComplexMatrix(circuits,elements):
-    print(elements)
-    matrix={}
+    """build complex matrix of circuits"""
+    
+    matrix=[]
     for elem in elements:
-        matrix[elem]=[]
+        temp=[]
         for c in circuits:
             if elem in circuits[c]:
-                matrix[elem].append(1)
+                temp.append(1)
             else:
-                matrix[elem].append(0)
+                temp.append(0)
+        matrix.append(temp)
     return matrix
 
 def buildConnMatrix(circuits,elements):
-    print(elements)
-    matrix={}
+    """Build connections matrix"""
+    
+    matrix=[]
     for elem in elements:
-        matrix[elem]=[]
+        temp=[]
         for el in elements:
             s=0
             if el!=elem:
                 for cir in circuits:
                     if elem in circuits[cir] and el in circuits[cir]:
                         s+=1
-            matrix[elem].append(s)
+            temp.append(s)
+        matrix.append(temp)
     return matrix
 
-lines=[
-    '5,4,3,2',
-    '4,8,3'
-   ]
-circuits=buildCircuits(lines)
-print(circuits)
-elements=set()
-for i in circuits:
-    for el in circuits[i]:
-        elements.add(el)
-elements=list(elements)
-elements.sort()
-matrix1=buildComplexMatrix(circuits,elements)
-temp=list(matrix1.keys())
-temp.sort()
-print(matrix1)
-matrix2=buildConnMatrix(circuits,elements)
-print(matrix2)
+def buildGraph(matrix,elements):
+    """Build graph from connections matrix"""
+    
+    i=0
+    graph={}
+    for row in matrix:
+        graph[elements[i]]=[]
+        k=-1
+        for el in row:
+            k+=1
+            if el==0:
+                continue
+            graph[elements[i]].append(elements[k])
+        i+=1
+    return graph
+
+def sortGraph(graph):
+    """Sort graph by not descending of nodes(amount of connected to node nodes)"""
+
+    #returns new OrderedDict
+    sorted=collections.OrderedDict()
+    length=len(graph)
+    #nodes which are not in sorted
+    keys=list(graph.keys())
+    while len(sorted)!=length:
+        maxlen=-1
+        maxind=0
+        for key in keys:
+            if len(graph[key])>maxlen:
+                maxlen=len(graph[key])
+                maxind=key
+        sorted[maxind]=graph[maxind]
+        keys.remove(maxind)
+    return sorted
+
+def colorGraph(graph):
+    """Colorise given graph"""
+    
+    j=1
+    colored={}
+    sorted=graph
+    while len(sorted)!=0:
+        colored[j]=[]
+        sorted=sortGraph(sorted)
+        (node,nodes)=sorted.popitem(False)
+        colored[j].append(node)
+        #coloring nodes
+        for nod in sorted:
+            if node in sorted[nod] and nod not in nodes:
+                print('bullshit',node,nod)
+                exit()
+            if nod in nodes and node not in sorted[nod]:
+                print('bullshit2',node,nod)
+                exit()
+            add=True
+            #check that nodes colored on this iteration not connected to nod
+            for n in colored[j]:
+                if n in sorted[nod]:
+                    add=False
+                    break
+            if add:
+                colored[j].append(nod)
+            #if node not in sorted[nod]:
+            #    colored[j].append(nod)
+        for key in colored[j]:
+            #deleting node
+            if key!=node:
+                del sorted[key]
+            #deleting all connections to this node
+            for nod in sorted:
+                if key in sorted[nod]:
+                    sorted[nod].remove(key)
+        j+=1
+    return colored
--- ktope_web.py
+++ ktope_web.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import cherrypy
+import ktope_hw2
 
 class KtoPage:
     
@@ -17,7 +18,10 @@ class KtoPage:
         background:#ccc;
     }
     .lightGray {
-        bacground:#eee;
+        background:#eee;
+    }
+    .white {
+        background:#fff;
     }
     </style>
     </head>
@@ -28,9 +32,9 @@ class KtoPage:
 </html>'''
 
     def index(self):
-        return self.index+'''
+        return self.header+'''
         На каждой строке файла по одной цепи, в цепи через запятую - модули.
-        <form action="result" method="GET">
+        <form action="/result" method="GET">
         <textarea rows="30" cols="80" name="data"></textarea><br />
         <input type="submit" /><br />
         Нажимая на эту кнопку, вы соглашаетесь, что пользуетесь этим сервисом на свой страх и риск
@@ -43,4 +47,101 @@ class KtoPage:
     def result(self,data=None):
         if not data:
             return self.header+'''Введите <a href="./">данные</a>'''+self.footer
-        
+        circuits=ktope_hw2.buildCircuits(data.split('\n'))
+        elements=set()
+        html='''<b>Исходные данные</b><br /><br />'''
+        for i in range(1,len(circuits)+1):
+            if len(circuits[i])<=1:
+                del circuits[i]
+                html+='Цепь №%i пустая <br />' % i
+                continue
+            html+='Цепь №%i %s<br />' % (i,str(circuits[i]))
+            for el in circuits[i]:
+                elements.add(el)
+        elements=list(elements)
+        elements.sort()
+        matrix1=ktope_hw2.buildComplexMatrix(circuits,elements)
+        width=int(100/(len(circuits)+1))
+        html+='''<br />
+        <b>Матрица комплексов</b><br /><br />
+        <table cellspacing="0">
+        <tr class="gray">
+            <td width="%i%%" class="white"></td>
+            ''' % width
+        for key in circuits:
+            html+='''<td width="%i%%"><b>%i</b></td>''' % (width,key)
+        html+='</tr>'
+        lgray=False
+        i=1
+        for row in matrix1:
+            if lgray:
+                html+='<tr class="lightGray">'
+            else:
+                html+='<tr>'
+            html+='<td class="gray"><b>%i</b></td>' % i
+            for el in row:
+                if el==0:
+                    html+='<td>%i</td>' % el
+                else:
+                    html+='<td><b>%i</b></td>' % el
+            html+='</tr>'
+            i+=1
+            lgray=not lgray
+        width=int(100/(len(elements)+1))
+        matrix2=ktope_hw2.buildConnMatrix(circuits,elements)
+        html+='''</table><br />
+        <b>Матрица соединений</b><br /><br />
+        <table cellspacing="0">
+        <tr class="gray">
+        <td width="%i%%" class="white">
+        ''' % width
+        for el in elements:
+            html+='<td width="%i%%"><b>%i</b></td>' % (width,el)
+        html+='</tr>'
+        i=0
+        lgray=False
+        for row in matrix2:
+            if lgray:
+                html+='<tr class="lightGray">'
+            else:
+                html+='<tr>'
+            html+='<td class="gray"><b>%i</b></td>' % elements[i]
+            k=0
+            for val in row:
+                if k==i:
+                    html+='<td><b>%i</b></td>' % val
+                else:
+                    html+='<td>%i</td>' % val
+                k+=1
+            html+='</tr>'
+            i+=1
+            lgray=not lgray
+        html+='''</table><br /><br />
+        <b>Раскраска графа</b><br /><br />
+        <table cellspacing="0">
+        <tr class="gray">
+        <td width="%i%%">Вершина</td>''' % width
+        for el in elements:
+            html+='<td width="%i%%"><b>%i</b></td>' % (width,el)
+        html+='''</tr>
+        <tr><td class="gray">Кол-во смежных</td>'''
+        graph=ktope_hw2.buildGraph(matrix2,elements)
+        for key in graph:
+            html+='<td>%i</td>' % len(graph[key])
+        html+='</tr></table><br /><br />'
+        colored=ktope_hw2.colorGraph(graph)
+        for key in colored:
+            html+='j=%i: %s<br />' % (key,str(colored[key]))
+        return self.header+html+self.footer
+    result.exposed=True
+
+    def ktope(self):
+        return self.index()
+    ktope.exposed=True
+
+import os.path
+tutconf = os.path.join(os.path.dirname(__file__), 'tutorial.conf')
+if __name__ == '__main__':
+    cherrypy.quickstart(KtoPage(), config=tutconf)
+else:
+    cherrypy.tree.mount(KtoPage(), config=tutconf)
